0de6c3467e6b2d8a8d3713f6b194392f1bf04008
flyicarus-net <ripollh@hotmail.com>
2023-03-10 18:03:29 +0100

    JS cambia atributos + salidas

---
+++ JS/cambia_atributo.js
@@ -0,0 +1,11 @@
+function cambiaColor() {
+    document.getElementById('estilo').style.color='blue';
+}
+
+function muestraFlecha() {
+    document.getElementById('mar').style.display='block';
+}
+
+function alineaDerecha() {
+    document.getElementById('estilo').style.textAlign='right';    
+}
