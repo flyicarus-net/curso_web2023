4568c6ded7d70172bcf7913367db1874a35927e6
flyicarus-net <ripollh@hotmail.com>
2023-04-12 15:56:55 +0200

    UF1842 PRACTICA 1

---
+++ UF1842_PRACTICA_1/comportament.js
@@ -0,0 +1,12 @@
+function func1() {
+   document.getElementById("paragraf").style.fontSize = "35px";
+};
+
+function func2() {
+    document.getElementById("titol").style.color = "red";
+};
+
+function func3() {
+    document.getElementById("canvia").innerHTML = "Exercici 5 resolt";
+};
+
